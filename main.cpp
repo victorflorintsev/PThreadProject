49dd95824f0980ebf6ec6dce15207c5ab6856a0d
victor <victorflorintsev1996@gmail.com>
2018-04-28 21:26:52 -0500

    added readfile method

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
 
 sem_t mutex;
 
@@ -20,9 +24,30 @@ void* thread(void* arg)
     sem_post(&mutex);
 }
 
+void* car(void* arg) {
+
+}
+
 
 int main()
 {
+    string filepath = "inputa.txt";
+    ifstream inFile;
+    inFile.open(filepath);
+    if (!inFile) {
+        cerr << "Unable to open file";
+        exit(1);
+    }
+    string x = "";
+    string input = "";
+    while (inFile >> x) {
+        input.append(x);
+        input.append("\n");
+    }
+    inFile.close();
+
+    cout << input;
+
     sem_init(&mutex, 0, 1);
     pthread_t t1,t2;
     pthread_create(&t1,NULL,thread,NULL);
