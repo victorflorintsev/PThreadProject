8f6d3fc6cd8297da5dd6eb7ceaa24ab9a38a380d
victor <victorflorintsev1996@gmail.com>
2018-04-30 14:25:05 -0500

    got threads to all start at once and end

--- main.cpp
+++ main.cpp
@@ -5,10 +5,30 @@
 #include <unistd.h>
 #include <fstream>
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
 sem_t mutex;
+
+enum tunnelstatus {WB, CLOSED, BB};
+
+class line {
+public:
+    line() {}
+    int timeIn;
+    bool WBound;
+    int duration;
+};
+
+struct Compareline {
+    bool operator()(line*& n1, line*& n2)
+    {
+        return n1->timeIn > n2->timeIn;
+    }
+};
+
+bool isWBound(string basic_string);
 
 void* thread(void* arg)
 {
@@ -25,7 +45,10 @@ void* thread(void* arg)
 }
 
 void* car(void* arg) {
-
+    line* in = (line*) arg;
+    cout << "sleeping for " << in->timeIn << "seconds." << endl;
+    sleep(in->timeIn);
+    cout << "stopped sleeping." << endl;
 }
 
 
@@ -38,23 +61,57 @@ int main()
         cerr << "Unable to open file";
         exit(1);
     }
+    int n = 0;
+    int test = 0;
     string x = "";
     string input = "";
-    while (inFile >> x) {
-        input.append(x);
-        input.append("\n");
+    inFile >> x;
+    int MAX_TUNNEL = stoi(x);
+    input.append(x);
+    input.append("\n");
+
+
+    try {
+        line *cur;
+        while (inFile >> x) {
+            cur = new line();
+            cur->timeIn = stoi(x);
+            input.append(x);
+            input.append(" ");
+            inFile >> x;
+            cur->WBound = isWBound(x);
+            input.append(x);
+            input.append(" ");
+            inFile >> x;
+            cur->duration = stoi(x);
+            input.append(x);
+            input.append("\n");
+
+            pthread_t carid;
+            pthread_create(&carid,NULL,car,(void *)cur);
+
+            n++;
+        }
+    } catch (exception e) {
+        cout << "something went wrong reading the file";
     }
+    pthread_exit(0);
     inFile.close();
 
     cout << input;
 
-    sem_init(&mutex, 0, 1);
-    pthread_t t1,t2;
-    pthread_create(&t1,NULL,thread,NULL);
-    sleep(2);
-    pthread_create(&t2,NULL,thread,NULL);
-    pthread_join(t1,NULL);
-    pthread_join(t2,NULL);
-    sem_destroy(&mutex);
+//    sem_init(&mutex, 0, 1);
+//    pthread_t t1,t2;
+//    pthread_create(&t1,NULL,thread,NULL);
+//    sleep(2);
+//    pthread_create(&t2,NULL,thread,NULL);
+//    pthread_join(t1,NULL);
+//    pthread_join(t2,NULL);
+//    sem_destroy(&mutex);
     return 0;
+}
+
+bool isWBound(string str) {
+    if (str == "WB") return true;
+    return false;
 }
